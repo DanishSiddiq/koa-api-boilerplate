bf7b574a178aecbc77a2023c12fa84a7625ccfd5
Danish Siddiq <mdanishsiddiq@yahoo.com>
2020-03-28 22:59:57 +0400

    develop: feat: adding type script

---
+++ src/models/interface/employee.d.ts
@@ -0,0 +1,11 @@
+export interface Employee {
+    firstName: String,
+    lastName: String,
+    email: String,
+    registrationNumber: String,
+    password: String,
+    passwordVerify: String,
+    isEnabled: Boolean,
+    createdAt: Date,
+    updatedAt: Date
+}
